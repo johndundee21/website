c107233e7086ec635d470189becd52615300f70d
nisarhassan12 <syednisarhassan12@gmail.com>
2021-07-28 00:57:05 +0500

    small fixes and updates

--- src/functions/feedback/index.ts
+++ src/functions/feedback/index.ts
@@ -1,22 +1,21 @@
 import type { Handler } from "@netlify/functions";
-import type { Response } from "@netlify/functions/src/function/response";
-import { submitFeedback as submitDocsGuidesFeedback } from "./_docs-guides";
 import { submitFeedback as submitExtensionFeedback } from "./_browser-extension";
+import { submitFeedback as submitManualFeedback } from "./_manual";
 
-const feedbackTypeToFunctionMap: {
-  [type: string]: (body: string) => Promise<Response>;
-} = {
-  docs: submitDocsGuidesFeedback,
-  guides: submitDocsGuidesFeedback,
-  "browser-extension": submitExtensionFeedback,
-  undefined: () => {
+const feedbackTypeToFunctionMap = (type: string) => {
+  if (type === "docs" || type === "guides") {
+    return submitManualFeedback;
+  } else if (type === "browser-extension") {
+    return submitExtensionFeedback;
+  }
+  return () => {
     throw new Error("Pleae provide a feedback type.");
-  },
+  };
 };
 
 const handler: Handler = (event, _, callback) => {
   console.log(JSON.stringify(event.body));
-  const submitFeedback = feedbackTypeToFunctionMap[JSON.parse(event.body).type];
+  const submitFeedback = feedbackTypeToFunctionMap(JSON.parse(event.body).type);
 
   submitFeedback(event.body)
     .then((response) => callback(null, response))
--- src/routes/guides/set-up-a-full-drupal-dev-environment-in-a-browser.md
+++ src/routes/guides/set-up-a-full-drupal-dev-environment-in-a-browser.md
@@ -4,7 +4,7 @@ date: Thursday, 4 Jun 2021 11:00:00 UTC
 title: Set up a full Drupal dev environment in a browser
 slug: set-up-a-full-drupal-dev-environment-in-a-browser
 excerpt: This project demonstrates a complete Drupal 9 development environment, utilizing ddev and Gitpod, through your browser.
-teaserImage: teaserImage.png
+teaserImage: teaser-image.png
 image: teaser.jpg
 ---
 
